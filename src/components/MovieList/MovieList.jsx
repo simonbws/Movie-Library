dfabfaa7af70b1f859632931f1ebcafe7fc7cd8e
simonbws <simonbws6@gmail.com>
2024-11-30 12:31:47 +0100

    Projects components and styles

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./MovieList.css";
+import Fire from "../../assets/fire.png";
+import MovieCard from "./MovieCard";
+
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=a880084d1089ac939e06aa96ff11de61"
+    );
+    const data = await response.json();
+    setMovies(data.results);
+  };
+  return (
+    <section claassName="movie_list">
+      <header className="align_center movie_list_header">
+        <h2 className="align_center movie_list_heading">
+          Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
+        </h2>
+        <div className="align_center movie_list_fs">
+          <ul className="align_center movie_filter">
+            <li className="movie_filter_item active">8+ Star</li>
+            <li className="movie_filter_item">7+ Star</li>
+            <li className="movie_filter_item">6+ Star</li>
+          </ul>
+
+          <select name="" id="" className="movie_sorting">
+            <option value="">SortBy</option>
+            <option value="">Date</option>
+            <option value="">Rating</option>
+          </select>
+          <select name="" id="" className="movie_sorting">
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
+          </select>
+        </div>
+      </header>
+      <div className="movie_cards">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default MovieList;
